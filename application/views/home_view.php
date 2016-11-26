a5b41187c755bf0b0a406ce450df85299a98d007
day2-p1 <palagorn.na@hotmail.com>
2016-11-26 16:28:54 +0700

    add footer

--- application/views/home_view.php
+++ application/views/home_view.php
@@ -1,46 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <title>mikky</title>
-    <link href="bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
-    <![endif]-->
-    <link rel="stylesheet" href="bower_components/font-awesome/css/font-awesome.min.css">
-    <link rel="stylesheet" href="bower_components/animate.css/animate.min.css">
-</head>
-<body>
-<nav class="navbar navbar-inverse">
-    <div class="container">
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="#">Project name</a>
-        </div>
-        <div id="navbar" class="navbar-collapse collapse">
-            <ul class="nav navbar-nav">
-                <li class="active"><a href="#">Home</a></li>
-                <li><a href="#about">About</a></li>
-                <li><a href="#contact">Contact</a></li>
-                <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-                    <ul class="dropdown-menu">
-                        <li><a href="#">Action</a></li>
-                        <li><a href="#">Another action</a></li>
-                        <li><a href="#">Something else here</a></li>
-                        <li role="separator" class="divider"></li>
-                        <li class="dropdown-header">Nav header</li>
-                        <li><a href="#">Separated link</a></li>
-                        <li><a href="#">One more separated link</a></li>
-                    </ul>
-                </li>
-            </ul>
-        </div>
-    </div>
-</nav>
+
 <div class="container">
     <h1>Training Responsive Web <small>Chiang Mai Rajabhat University</small></h1>
     <hr class="divider">
@@ -128,9 +86,3 @@
         </ul>
     </div>
 </div>
-<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-<script src="bower_components/jquery/dist/jquery.min.js"></script>
-<!-- Include all compiled plugins (below), or include individual files as needed -->
-<script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
-</body>
-</html>
---
+++ application/views/test_view.php
@@ -0,0 +1,10 @@
+<div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae consequuntur deleniti distinctio dolorum eaque
+    earum eligendi facere impedit magnam, maxime minus officia placeat quibusdam rem soluta temporibus veritatis
+    voluptatem voluptatum.
+</div>
+<div>Adipisci alias architecto asperiores at aut cupiditate dolorem dolorum ducimus facilis fuga illo ipsa iure iusto
+    magni natus numquam obcaecati porro quas recusandae repudiandae sequi sunt, ullam velit veritatis voluptates!
+</div>
+<div>Asperiores dolorem eos hic, iste maiores modi reiciendis! Asperiores dolore dolores error excepturi facere ipsum
+    modi nemo vel? Accusantium aperiam at commodi corporis eius laborum quo totam voluptas. Adipisci, voluptatum!
+</div>
