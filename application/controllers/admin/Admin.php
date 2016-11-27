fdf8476e854139d25d5bc5d1d140233f99ac04d2
day2-p1 <palagorn.na@hotmail.com>
2016-11-27 14:52:14 +0700

    เริ่ม ทำฟรอม

--- application/controllers/admin/Admin.php
+++ application/controllers/admin/Admin.php
@@ -4,12 +4,51 @@
  * Created by PhpStorm.
  * User: lab
  * Date: 27/11/2559
- * Time: 11:17
+ * Time: 10:45
  */
 class Admin extends CI_Controller
 {
     public function index()
     {
+        $this->load->view('admin/layout/admin_header_view');
+        $this->load->view('admin/layout/admin_sidebar_view');
         $this->load->view('admin/admin_view');
+        $this->load->view('admin/layout/admin_footer_view');
+    }
+    public function category()
+    {
+        $this->load->view('admin/layout/admin_header_view');
+        $this->load->view('admin/layout/admin_sidebar_view');
+        $this->load->view('admin/category_view');
+        $this->load->view('admin/layout/admin_footer_view');
+    }
+    public function add_category()
+    {
+        $this->load->view('admin/layout/admin_header_view');
+        $this->load->view('admin/layout/admin_sidebar_view');
+        $this->load->view('admin/add_category_view');
+        $this->load->view('admin/layout/admin_footer_view');
+    }
+
+    public function news()
+    {
+        $this->load->view('admin/layout/admin_header_view');
+        $this->load->view('admin/layout/admin_sidebar_view');
+        $this->load->view('admin/news_view');
+        $this->load->view('admin/layout/admin_footer_view');
+    }
+    public function comment()
+    {
+        $this->load->view('admin/layout/admin_header_view');
+        $this->load->view('admin/layout/admin_sidebar_view');
+        $this->load->view('admin/comment_view');
+        $this->load->view('admin/layout/admin_footer_view');
+    }
+    public function user()
+    {
+        $this->load->view('admin/layout/admin_header_view');
+        $this->load->view('admin/layout/admin_sidebar_view');
+        $this->load->view('admin/user_view');
+        $this->load->view('admin/layout/admin_footer_view');
     }
 }
---
+++ application/views/admin/category_view.php
@@ -0,0 +1,37 @@
+<!-- Page Content -->
+<div id="page-wrapper">
+    <div class="row">
+        <div class="col-lg-12">
+
+            <h1 class="page-header">จัดการหมวดหมู่ข่าว</h1>
+        </div>
+        <!-- /.col-lg-12 -->
+        <div class="col-lg-12">
+        <table class="table">
+            <tr>
+                <div class="text-right">
+                <a href="<?php echo base_url('admin/add_category')?>" class="btn btn-xs btn-primary">
+                <i class="fa fa-plus"></i>เพิ่มหมวดหมู่
+                </a>
+                <th>#</th>
+                <th>ชื่อหมวดหมู่</th>
+                <th>จัดการ</th>
+            </tr>
+            <tr>
+                <td>1</td>
+                <td>กีฬา</td>
+                <td>
+                    <a href="#" class="btn btn-xs btn-warning">
+                       <i class="fa fa-edit">แก้ไข</i>
+                    </a>
+                    <a href="#" class="btn btn-xs btn-danger">
+                    <i class="fa fa-edit">ลบ</i>
+                    </a>
+                </td>
+            </tr>
+        </table>
+        </div>
+    </div>
+    <!-- /.row -->
+</div>
+<!-- /#page-wrapper -->
